72532af15bfeda6bbde6e006f30ff0b64db3ca12
raissapequeno <raissa.prado@aluno.ifsp.edu.br>
2023-08-16 11:19:06 -0300

    Configurado banco MySQL

--- webapp/settings.py
+++ webapp/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'django_blog_db',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
 }
+
 
 
 # Password validation
