1c6c10512b1dd8964bda26b5c87c23ce4a9f0bac
raissapequeno <raissa.prado@aluno.ifsp.edu.br>
2023-08-30 11:12:11 -0300

    30/08

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from blog.views import index, ola
+from blog.views import index, ola, post_show, PostDetailView
 
 urlpatterns = [
-    path('index/', index, name= "index"),
-    path('ola/', ola, name="ola")
+    path("index/", index, name="index"),
+    path("ola/", ola, name="ola"),
+    path("posts/all", ola, name="post_list"),
+    path("post/<int:post_id>", post_show, name="exibe_post"),
+    path("post/<int:pk>/show", PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Inclui a classe HttpResponse
 from django.http import HttpResponse
 
+from django.views.generic.detail import DetailView
+
+from blog.models import Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post/detail.html"
+    context_object_name = "post"
+
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "post/detail.html", {"post": post})
+
+
 # Define uma function view chamada index
 def index(request):
     # return HttpResponse('Olá Django - index')
-    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+    return render(request, "index.html", {"titulo": "Últimos Artigos"})
 
 
 # Define uma function view chamada ola.
 def ola(request):
     # return HttpResponse('Olá Django')
     # return render(request, 'index.html')
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+    posts = Post.objects.all()  # recupera todos os posts do banco de dados
+    context = {"posts_list": posts}  # cria um dicionário com os dado
+    return render(
+        request, "posts.html", context
+    )  # renderiza o template e passa o contexto
