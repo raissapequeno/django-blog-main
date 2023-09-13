cfc95d1bcb6c0fcfb0be89b9de1722609fc067e4
raissapequeno <raissa.prado@aluno.ifsp.edu.br>
2023-09-13 10:42:31 -0300

    Respostas JSON para o model Post.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from blog.views import index, ola, post_show, PostDetailView
+from blog.views import (index, ola, post_show, PostDetailView, get_all_posts, get_post)
+
 
 urlpatterns = [
-    path("index/", index, name="index"),
-    path("ola/", ola, name="ola"),
-    path("posts/all", ola, name="post_list"),
-    path("post/<int:post_id>", post_show, name="exibe_post"),
-    path("post/<int:pk>/show", PostDetailView.as_view(), name="post_detail"),
+    path('index/', index, name='index'),
+    path('ola/', ola, name='ola' ),
+    path('posts/all', ola, name='posts_list'),
+    path('post/<int:post_id>', post_show, name="exibe_post"),
+    path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 
-# Inclui a classe HttpResponse
+# Create your views here.
 from django.http import HttpResponse
 
 from django.views.generic.detail import DetailView
 
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 from blog.models import Post
-
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "post/detail.html"
-    context_object_name = "post"
-
 
 def post_show(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, "post/detail.html", {"post": post})
+    return render(request, 'post/detail.html', {'post': post})
 
-
-# Define uma function view chamada index
 def index(request):
-    # return HttpResponse('Olá Django - index')
-    return render(request, "index.html", {"titulo": "Últimos Artigos"})
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
 
-# Define uma function view chamada ola.
-def ola(request):
-    # return HttpResponse('Olá Django')
-    # return render(request, 'index.html')
-    # return render(request, 'home.html')
-    posts = Post.objects.all()  # recupera todos os posts do banco de dados
-    context = {"posts_list": posts}  # cria um dicionário com os dado
-    return render(
-        request, "posts.html", context
-    )  # renderiza o template e passa o contexto
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
+
+def get_all_posts(request):
+    posts = list(Post.objects.values('pk', 'body_text', 'pub_date'))
+    data = {'success': True, 'posts': posts}
+    json_data = json.dumps(data, indent=1, cls=DjangoJSONEncoder)
+    response = HttpResponse(json_data, content_type='application/json')
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    return response
+
+def get_post(request, post_id):
+    post = Post.objects.filter(pk=post_id).values('pk', 'body_text', 'pub_date').first()
+    data = {'success': True, 'post': post}
+    status = 200
+    if post is None:
+        data = {'success': False, 'error': 'Post ID não existe.'}
+        status=404
+    response = HttpResponse(
+        json.dumps(data, indent=1, cls=DjangoJSONEncoder),
+        content_type="application/json",
+        status=status
+    )
+    response['Access-Control-Allow-Origin'] = '*' # requisição de qualquer origem
+    
+    return response
